7724a2f8e8914d6d3938f11423636803c4b6df3b
kathrikiramanjaneyulu <kathrikiramanjaneyulu@gmail.com>
2025-08-20 12:18:14 +0530

    Update TestRunner.java

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -4,6 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
 //kljklasjfhsfuhsufhs;
+//xcv xcv xcvx
 @CucumberOptions(
         features = "src/test/java/features",
         glue = "stepdefinitions",
